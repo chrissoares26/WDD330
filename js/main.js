c546b6dc2e1076db43336c8b448219787a75c0ac
chrissoares26 <sou20001@byui.edu>
2021-04-22 10:39:16 -0600

    created json and js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const requestURL = "js/list.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const links = jsonObject["links"];
+
+    for (let i = 0; i < links.length; i++) {
+      let listitem = document.createElement("li");
+      let anchor = document.createElement("a");
+
+      anchor.setAttribute("href", links[i].url);
+      anchor.innerText = links[i].label;
+      listitem.appendChild(anchor);
+
+      document.querySelector("#links").appendChild(listitem);
+    }
+  });
