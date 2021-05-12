55f2c5b60436a07a398b38b478ce290ff151f67d
chrissoares26 <sou20001@byui.edu>
2021-05-12 14:34:43 -0600

    portfolio

--- notes/week4/script.js
+++ notes/week4/script.js
@@ -10,12 +10,8 @@ form.addEventListener('submit', (e) => {
         messages.push('Name is required')
     }
 
-    if (password.value.length <= 6) {
-        messages.push('Password must be longer than 6 characters')
-    }
-
-    if (password.value.length >= 20) {
-        messages.push('Password must be less than 20 characters')
+    if (password.value.length <= 6 || password.value.length >= 20 ) {
+        messages.push('Password must be between 6 and 20 characters')
     }
 
     if (password.value === 'password') {
